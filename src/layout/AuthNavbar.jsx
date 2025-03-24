e1e1f27e5d8aba2ccb049247210a686aef7158dd
Yusif Abbaszade <yusif@yusif.yusif>
2025-03-24 07:29:42 -0400

    SAF

--- src/layout/AuthNavbar.jsx
+++ src/layout/AuthNavbar.jsx
@@ -10,10 +10,7 @@ const AuthNavbar = () => {
         <div className="authnavbar-sec pt-4">
             <div className="container d-flex justify-content-between align-items-center">
                 <div className="left-sec d-flex gap-5">
-                    <NavLink to="/my-account/orders" className="nav-link">ORDERS</NavLink>
                     <NavLink to="/accountdetails" className="nav-link">YOUR DETAILS</NavLink>
-                    <NavLink to="/my-account/stock-notifications" className="nav-link">STOCK NOTIFICATIONS</NavLink>
-                    <NavLink to="/my-account/exclusive-offers" className="nav-link">EXCLUSIVE OFFERS</NavLink>
                 </div>
                 <div className="right-sec">
                     <button className="btn nav-link" onClick={()=>{setAuthData({"isAuth":false}); googleLogout(); navigete('/login')}}>LOG OUT</button>
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     const [shopddMenuOpen, setshopddMenuOpen] = useState(false)
     const [authData] = useContext(AuthContext);
     const [navbarTheme, setNavbarTheme] = useContext(NavbarContext);
-    const products = useSelector(p=>p.products);
+    const products = useSelector(p => p.products);
     const [productSearchText, setProductSearchText] = useState('');
     const navigate = useNavigate();
 
@@ -53,13 +53,8 @@ const Navbar = () => {
                     <Link to={'/'} style={{ width: "75px" }}><img src={navbarTheme === 'light' ? logo_white : logo_black} alt="" width={"100%"} /></Link>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0 gap-5">
-                            <li className="d-flex">
+                            <li>
                                 <Link className={`resetAllClass ${navbarTheme === 'light' ? '' : 'text-dark'}`} to={'/shop'}>Shop <div className="underline"></div></Link>
-                                <button className="" onClick={shopddToggle} style={{ background: "none", border: "none", marginBottom: "10px" }}>
-                                    <IconContext.Provider value={{ size: "1.4em", color: navbarTheme === 'light' ? "white" : "black", className: "" }}>
-                                        <IoIosArrowDown />
-                                    </IconContext.Provider>
-                                </button>
                             </li>
                             <li>
                                 <Link className={`resetAllClass ${navbarTheme === 'light' ? '' : 'text-dark'}`} to={'/about-us'}>About<div className="underline"></div></Link>
@@ -82,9 +77,9 @@ const Navbar = () => {
                                             <div className="d-flex flex-row w-100" style={{ height: "50px" }}>
                                                 <input type="text" style={{ width: "100%" }} className="border-1 ps-3" onChange={(e) => { setProductSearchText(e.target.value) }} />
                                             </div>
-                                            <div className="row" style={{maxHeight:"500px", overflowY:"scroll"}}>
+                                            <div className="row" style={{ maxHeight: "500px", overflowY: "scroll" }}>
                                                 {products.filter(p => p.title.toLowerCase().includes(productSearchText.toLowerCase())).map((item, index) => (
-                                                    <div data-bs-toggle='modal' onClick={()=>{navigate(`/shop/${slugify(item.title, {lower:true})}`)}} key={index} data-bs-target='#newsSearchModal' className="col-12 my-2 d-flex justify-content-around align-items-center" style={{cursor:"pointer"}}>
+                                                    <div data-bs-toggle='modal' onClick={() => { navigate(`/shop/${slugify(item.title, { lower: true })}`) }} key={index} data-bs-target='#newsSearchModal' className="col-12 my-2 d-flex justify-content-around align-items-center" style={{ cursor: "pointer" }}>
                                                         <div className="col-4">
                                                             <img src={item.img} alt="" width={"100%"} style={{ height: "10vh", objectFit: "cover" }} />
                                                         </div>
@@ -112,158 +107,18 @@ const Navbar = () => {
             </nav>
             <div className={`left-dd-navbar container-fluid ${leftMenuOpen ? 'navbar-dd-fade-anm-open' : 'd-none'}`} style={{ background: "white", position: "absolute", top: "10%", zIndex: "1" }} >
                 <ul className="list-unstyled">
-                    <li>
-                        <div className="d-flex">
-                            <Link onClick={resetAll} to={'/shop'} className="resetAllClass"><b>Shop</b></Link>
-                            <button className="accordion-button navmenuddbtn" style={{ fontSize: "18px" }} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                <IconContext.Provider value={{}}><IoIosArrowDown /></IconContext.Provider>
-                            </button>
-                        </div>
-                        <div className="accordion left-dd-shop-dd" id="accordionExample">
-                            <div className="accordion-item border-0">
-                                <div id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                                    <div className="accordion-body">
-                                        <h6>COLLECTIONS</h6>
-                                        <ul>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                        </ul>
-                                        <h6>ACTIVITIES</h6>
-                                        <ul>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                        </ul>
-                                        <h6>CATEGORIES</h6>
-                                        <ul>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <li>
-                                                <Link>example</Link>
-                                            </li>
-                                            <Link to={'/'} className="fw-bold fs-6">VIEW ALL PRODUCTS</Link>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-
+                    <li className="resetAllClass">
+                        <Link onClick={resetAll} to={'/shop'} className="resetAllClass"><b>Shop</b></Link>
                     </li>
                     <hr />
                     <li className="resetAllClass">
-                        <Link onClick={resetAll} to={'/about-us'}><b>About</b></Link>
+                        <Link onClick={resetAll} to={'/about-us'} className="resetAllClass"><b>About</b></Link>
                     </li>
                     <hr />
                     <li className="resetAllClass">
-                        <Link onClick={resetAll} to={'/news'}><b>News</b></Link>
+                        <Link onClick={resetAll} to={'/news'} className="resetAllClass"><b>News</b></Link>
                     </li>
                 </ul>
-            </div>
-            <div className={`shop-dd-menu ${shopddMenuOpen ? 'navbar-dd-fade-anm-open' : 'd-none'} `}>
-                <div className="col-5 links d-flex justify-content-around">
-                    <div className="col-1"></div>
-                    <div className="col-3">
-                        <legend className="fs-6" style={{ fontWeight: "bold" }}>COLLECTIONS</legend>
-                        <ul>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col-3">
-                        <legend className="fs-6" style={{ fontWeight: "bold" }}>ACTIVITIES</legend>
-                        <ul>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col-3">
-                        <legend className="fs-6" style={{ fontWeight: "bold" }}>CATEGORIES</legend>
-                        <ul>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                            <li>
-                                <Link>example</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="line-down me-5"></div>
-                <div className="col-5 right-side">
-                    <Link to={'/'} className="col-5 link1"></Link>
-                    <Link to={'/'} className="col-5 link2"></Link>
-                    <div className="col-2"></div>
-                </div>
             </div>
         </>
 
